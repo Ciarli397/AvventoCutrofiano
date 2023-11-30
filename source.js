fe77ba2ebe8f6f6163b8b343d571345229ae0308
Ciarli397 <119630812+Ciarli397@users.noreply.github.com>
2023-11-30 23:55:32 +0100

    Primo caricamento

---
+++ source.js
@@ -0,0 +1,129 @@
+    const source = [
+
+    {/* casella 1*/
+        "type": "image",
+        "icon": "ico-albero.png",
+        "day": "Giorno 1"
+    },
+    
+    {/* casella 2*/
+        "type": "image",
+        "icon": "ico-calza.png",
+        "day": "Giorno 2"
+    },
+    {/* casella 3*/
+        "type": "text",
+        "icon": "ico-pupazzo.png",
+        "day": "Giorno 3"
+    },
+    {/* casella 4*/
+        "type": "image",
+        "icon": "ico-fiocco.png",
+        "day": "Giorno 4"
+    },
+    {/* casella 5*/
+        "type": "text",
+        "icon": "ico-omino.png",
+        "day": "Giorno 5"
+    },
+    {/* casella 6*/
+        "type": "image",
+        "icon": "ico-stella.png",
+        "day": "Giorno 6"
+    },
+    {/* casella 7*/
+        "type": "text",
+        "icon": "ico-campana.png",
+        "day": "Giorno 7"
+    },
+    {/* casella 8*/
+        "type": "text",
+        "icon": "ico-caramella.png",
+        "day": "Giorno 8"
+    },
+    {/* casella 9*/
+        "type": "image",
+        "icon": "ico-albero.png",
+        "day": "Giorno 9"
+    },
+    {/* casella 10*/
+        "type": "text",
+        "icon": "ico-calza.png",
+        "day": "Giorno 10"
+    },
+    {/* casella 11*/
+        "type": "text",
+        "icon": "ico-pupazzo.png",
+        "day": "Giorno 11"
+    },
+    {/* casella 12*/
+        "type": "text",
+        "icon": "ico-fiocco.png",
+        "day": "Giorno 12"
+    },
+    {/* casella 13*/
+        "type": "text",
+        "icon": "ico-omino.png",
+        "day": "Giorno 13"
+    },
+    {/* casella 14*/
+        "type": "image",
+        "icon": "ico-stella.png",
+        "day": "Giorno 14"
+    },
+    {/* casella 15*/
+        "type": "text",
+        "icon": "ico-albero.png",
+        "day": "Giorno 15"
+    },
+    {/* casella 16*/
+        "type": "image",
+        "icon": "ico-calza.png",
+        "day": "Giorno 16"
+    },
+    {/* casella 17*/
+        "type": "text",
+        "icon": "ico-pupazzo.png",
+        "day": "Giorno 17"
+    },
+    {/* casella 18*/
+        "type": "text",
+        "icon": "ico-fiocco.png",
+        "day": "Giorno 18"
+    },
+    {/* casella 19*/
+        "type": "text",
+        "icon": "ico-omino.png",
+        "day": "Giorno 19"
+    },
+    {/* casella 20*/
+        "type": "text",
+        "icon": "ico-stella.png",
+        "day": "Giorno 20"
+    },
+    {/* casella 21*/
+        "type": "image",
+        "icon": "ico-albero.png",
+        "day": "Giorno 21"
+    },
+    {/* casella 22*/
+        "type": "image",
+        "icon": "ico-calza.png",
+        "day": "Giorno 22"
+    },
+    {/* casella 23*/
+        "type": "image",
+        "icon": "ico-pupazzo.png",
+        "day": "Giorno 23"
+    },
+    {/* casella 24*/
+        "type": "text",
+        "icon": "ico-fiocco.png",
+        "day": "Giorno 24"
+    },
+    {/* casella 25*/
+        "type": "image",
+        "icon": "ico-neve.png",
+        "day": "Giorno 25"
+    }
+];
